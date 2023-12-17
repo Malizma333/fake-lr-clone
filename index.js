042555dedf4f1cc3391bad3c76796e263e09721e
tkbessler <tkbessler@gmail.com>
2023-12-16 20:58:42 -0600

    gravity boilerplate

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+window.store.getState().camera.playbackFollower._frames.length = 0;
+window.store.getState().simulator.engine.engine._computed._frames.length = 1;
+window.store.dispatch({ type: "SET_PLAYER_STOP_AT_END", payload: false });
+window.store.dispatch({ type: "SET_PLAYER_MAX_INDEX", payload: 1 });
+window.store.dispatch({ type: 'SET_PLAYBACK_DIMENSIONS', payload: {width:1920, height:1080} });
+window.store.dispatch({ type: 'SET_VIEW_OPTION', payload: {key: 'playbackPreview', value: true} });
+window.store.dispatch({ type: 'SET_AUDIO_OFFSET', payload: 0 });
+window.store.dispatch({ type: 'SET_INTERPOLATE', payload: false });
 const KeyframeLR = (function() {
   const CONTROLS = {
     SPEED_UP: {KEY: 'w', state: 0},
@@ -9,11 +17,18 @@ const KeyframeLR = (function() {
     SNAP: {KEY: 'Shift', state: 0}
   };
 
-  const STATE = {
+  const MOVE_STATE = {
     speed: 10,
     rotation: 0,
     direction: 0
   };
+
+  const GRAVITY = {
+    DEFAULT: {x:0, y:0.175},
+    ZERO: {x:0, y:0},
+    currentSubframe: 0,
+    currentPointIndex: 0
+  }
 
   document.addEventListener('keydown', (event) => {
     switch(event.key) {
@@ -70,4 +85,25 @@ const KeyframeLR = (function() {
         break;
     }
   }, false);
+
+  Object.defineProperty(window.$ENGINE_PARAMS, "gravity", { get() {
+    try {
+      GRAVITY.currentSubframe += 1;
+      GRAVITY.currentPointIndex = GRAVITY.currentSubframe % 17;
+
+      if(GRAVITY.currentPointIndex > 10) return GRAVITY.DEFAULT;
+
+      const frames = store.getState().simulator.engine.engine._computed._frames;
+      const riderPoints = frames[frames.length-1].snapshot.entities[0].entities[0].points;
+      const currentPoint = riderPoints[GRAVITY.currentPointIndex];
+      const velocityCancel = {
+        x: currentPoint.pos.x + currentPoint.vel.x,
+        y: currentPoint.pos.y + currentPoint.vel.y
+      };
+
+      return GRAVITY.ZERO;
+    } catch(e) {
+      console.error(e);
+    }
+  }});
 })();
